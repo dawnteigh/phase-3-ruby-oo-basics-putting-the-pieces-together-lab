89443f233606f10457e126e805aa1fdf36884577
Donte Ladatto <donte279@gmail.com>
2022-05-30 17:55:58 -0500

    commit tree fb76160037451fd620615a8b6d8764c9c75af076

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,12 @@
-
+class Book
+    attr_reader :title
+    attr_accessor :author, :page_count, :genre
+    def initialize(title = "Why Dirt Is My Favorite Food")
+        @title = title
+    end
+    def turn_page
+        #puts "My, how the turn-pages... turn"
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
